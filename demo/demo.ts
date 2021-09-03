badea3d2c20274efadd96c823ecc99f6e96ecfdc
Baku Hashimoto <mail@baku89.com>
2021-09-03 18:43:36 +0900

    Update demo

--- demo/demo.ts
+++ demo/demo.ts
@@ -1,3 +1,30 @@
-import {test} from '../src/index'
+const initDevice = async () => {
+	let [device] = await navigator.usb.getDevices()
 
-test()
+	if (!device) {
+		device = await navigator.usb.requestDevice({filters: []})
+	}
+
+	await device.open()
+
+	console.log(`device=${device.productName}`)
+
+	if (!device.configuration) {
+		await device.selectConfiguration(1)
+	}
+	
+	await device.claimInterface(0)
+
+	console.log('Claim succeed')
+
+	await device.transferOut(1, Uint16Array.of(0x9035, 0x0, 0x0, 0x0, 0x0, 0x00))
+	console.log('sent')
+
+	const res = await device.transferIn(2, 1)
+
+	console.log('received', res)
+}
+
+
+document.getElementById('execute')?.addEventListener('click', initDevice)
+
