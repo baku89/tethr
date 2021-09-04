a348c9b0e9116cf67525ac5e8df133d6c287d57c
Baku Hashimoto <mail@baku89.com>
2021-09-05 07:54:38 +0900

    Update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,10 +6,7 @@ module.exports = {
 		commonjs: true,
 	},
 	parser: '@typescript-eslint/parser',
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-	],
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020,
