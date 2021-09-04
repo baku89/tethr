fb6e2ae38599dfb702e68c36e11e9066e7748ad6
Baku Hashimoto <mail@baku89.com>
2021-09-05 07:55:28 +0900

    Refactor demo with using PTPDevice

--- demo/demo.ts
+++ demo/demo.ts
@@ -1,30 +1,121 @@
+import {PTPDecoder} from '../src/PTPDecoder'
+import {PTPDevice} from '../src/PTPDevice'
+import {
+	DevicePropCodeTable,
+	EventTypeTable,
+	ObjectFormatCodeTable,
+	OperationCodeTable,
+} from '../src/Table'
+
 const initDevice = async () => {
-	let [device] = await navigator.usb.getDevices()
+	const device = new PTPDevice()
+	await device.connect()
 
-	if (!device) {
-		device = await navigator.usb.requestDevice({filters: []})
+	const GetDeviceInfo = 0x1001
+	const OpenSession = 0x1002
+	const GetStorageIDs = 0x1004
+	const GetStorageInfo = 0x1005
+	const CloseSession = 0x1003
+	const ResetDevice = 0x1010
+
+	{
+		const result = await device.performTransaction({
+			operationCode: GetDeviceInfo,
+		})
+		if (!result.data) throw new Error()
+		const decoder = new PTPDecoder(result.data)
+
+		console.log('deviceInfo=', {
+			StandardVersion: decoder.getUint16(),
+			VendorExtensionID: decoder.getUint32(),
+			VendorExtensionVersion: decoder.getUint16(),
+			VendorExtensionDesc: decoder.getString(),
+			FunctionalMode: decoder.getUint16(),
+			OperationsSupported: decoder.getUint16Array(
+				getCodeTransformer(OperationCodeTable)
+			),
+			EventsSupported: decoder.getUint16Array(
+				getCodeTransformer(EventTypeTable)
+			),
+			DevicePropertiesSupported: decoder.getUint16Array(
+				getCodeTransformer(DevicePropCodeTable)
+			),
+			CaptureFormats: decoder.getUint16Array(
+				getCodeTransformer(ObjectFormatCodeTable)
+			),
+			ImageFormats: decoder.getUint16Array(
+				getCodeTransformer(ObjectFormatCodeTable)
+			),
+			Manufacturer: decoder.getString(),
+			Model: decoder.getString(),
+			DeviceVersion: decoder.getString(),
+			SerialNumber: decoder.getString(),
+		})
+
+		await device.performTransaction({
+			operationCode: OpenSession,
+			parameters: [0x1],
+		})
 	}
 
-	await device.open()
+	// await sendCommand(0x9102, [0x00010001])
+	// await receiveResponse()
 
-	console.log(`device=${device.productName}`)
+	// Shutter
+	await device.performTransaction({
+		operationCode: 0x9404,
+		parameters: [0x3000011],
+	})
 
-	if (!device.configuration) {
-		await device.selectConfiguration(1)
-	}
-	
-	await device.claimInterface(0)
+	// await sendCommand(0x9103, [])
+	// await receiveResponse()
 
-	console.log('Claim succeed')
+	// await sendCommand(0x9035, [0x0])
+	// await receiveResponse()
 
-	await device.transferOut(1, Uint16Array.of(0x9035, 0x0, 0x0, 0x0, 0x0, 0x00))
-	console.log('sent')
+	// await sendCommand(0x9012, [0x0])
+	// await receiveResponse()
 
-	const res = await device.transferIn(2, 1)
+	await device.performTransaction({
+		operationCode: GetStorageIDs,
+	})
+	// const storageIDs = (await receiveResponse()).getUint16Array()
+	// await receiveResponse()
+	// console.log('storage ids =', storageIDs)
 
-	console.log('received', res)
+	// await sendCommand(GetStorageInfo, [x])
+	// const storageInfo = await receiveResponse()
+	// await receiveResponse()
+
+	// console.log('storage info=', {
+	// 		storageType: storageInfo.getUint16(),
+	// 		filesystemType: storageInfo.getUint16(),
+	// 		accessCapability: storageInfo.getUint16(),
+	// 		maxCapability: storageInfo.getUint64(),
+	// 		freeSpaceInBytes: storageInfo.getUint64(),
+	// 		freeSpaceInImages: storageInfo.getUint32(),
+	// 	},
+	// )
+
+	await device.performTransaction({
+		operationCode: CloseSession,
+	})
+
+	device.close()
+
+	// await sendCommand(0x901b, [0x06])
+	// await sendData(0x901b)
+	// await receiveResponse()
 }
 
+function getCodeTransformer(table: Record<number, string>) {
+	return (code: number) => {
+		if (code in table) {
+			return table[code]
+		} else {
+			return '0x' + ('0000' + code.toString(16)).slice(-4)
+		}
+	}
+}
 
 document.getElementById('execute')?.addEventListener('click', initDevice)
-
