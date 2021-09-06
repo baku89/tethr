b1c5a924e6e96a5eeaffec1075381ec3398492d2
Baku Hashimoto <mail@baku89.com>
2021-09-06 23:26:01 +0900

    Move extractJpeg definition

--- src/CameraControl/CameraControl.ts
+++ src/CameraControl/CameraControl.ts
@@ -267,4 +267,37 @@ export class CameraControl {
 
 		return info
 	}
+
+	protected static extractJpeg(buffer: ArrayBuffer): ArrayBuffer {
+		const bytes = new Uint8Array(buffer)
+		const len = bytes.length
+
+		// look for the JPEG SOI marker (0xFFD8) in data
+		let start: null | number = null
+
+		for (let i = 0; i + 1 < len; i++) {
+			if (bytes[i] === 0xff && bytes[i + 1] === 0xd8) {
+				// SOI found
+				start = i
+				break
+			}
+		}
+		if (start === null) /* no SOI -> no JPEG */ throw new Error('SOI not found')
+
+		// look for the JPEG SOI marker (0xFFD8) in data
+		let end: null | number = null
+
+		for (let i = start + 2; i + 1 < len; i++) {
+			if (bytes[i] === 0xff && bytes[i + 1] === 0xd9) {
+				// EOI found
+				end = i + 2
+				break
+			}
+		}
+		if (end === null)
+			// no EOI -> no JPEG
+			throw new Error('EOI not found')
+
+		return buffer.slice(start, end)
+	}
 }
