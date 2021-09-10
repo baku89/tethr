71664d0fa2ef196594a86d88fc9be423895dbeed
Baku Hashimoto <mail@baku89.com>
2021-09-11 03:26:10 +0900

    Add PTPDatacode

--- src/PTPDatacode.ts
+++ src/PTPDatacode.ts
@@ -149,3 +149,28 @@ export enum DevicePropCode {
 	Artist = 0x501e,
 	CopyrightInfo = 0x501f,
 }
+
+export enum DatatypeCode {
+	Undef = 0x0000,
+	Int8 = 0x0001,
+	Uint8 = 0x0002,
+	Int16 = 0x0003,
+	Uint16 = 0x0004,
+	Int32 = 0x0005,
+	Uint32 = 0x0006,
+	Int64 = 0x0007,
+	Uint64 = 0x0008,
+	Int128 = 0x0009,
+	Uint128 = 0x000a,
+	Int8Array = 0x4001,
+	Uint8Array = 0x4002,
+	Int16Array = 0x4003,
+	Uint16Array = 0x4004,
+	Int32Array = 0x4005,
+	Uint32Array = 0x4006,
+	Int64Array = 0x4007,
+	Uint64Array = 0x4008,
+	Int128Array = 0x4009,
+	Uint128Array = 0x400a,
+	String = 0xffff,
+}
