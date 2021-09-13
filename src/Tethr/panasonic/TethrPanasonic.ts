ce95799581c2cc9c33801e177684ac57c42dbc8e
Baku Hashimoto <mail@baku89.com>
2021-09-13 17:58:50 +0900

    Fix ImageQualityTable

--- src/Tethr/panasonic/TethrPanasonic.ts
+++ src/Tethr/panasonic/TethrPanasonic.ts
@@ -587,8 +587,8 @@ export class TethrPanasnoic extends Tethr {
 	private static ImageQualityTable = new BiMap<number, string>([
 		[0, 'fine'],
 		[1, 'std'],
-		[2, 'raw + fine'],
-		[3, 'raw + std'],
-		[4, 'raw'],
+		[2, 'raw'],
+		[3, 'raw +fine'],
+		[4, 'raw + std'],
 	])
 }
