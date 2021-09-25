c3f75e9aa79c47bd4fb7daefaf2fe7b2dc3dfdd7
Baku Hashimoto <mail@baku89.com>
2021-09-25 14:35:48 +0900

    Add write option for PTPDataView

--- src/PTPDataView.ts
+++ src/PTPDataView.ts
@@ -1,81 +1,91 @@
 import {parse as parseTimestamp} from 'date-format-parse'
 import _ from 'lodash'
 
+const DEFAULT_BUFFER_SIZE = 2048
+
 /**
- * DataView's subclass with some utility methods for PTP packet decoding
+ * DataView-like class that adds automatic read & write offset tracking, string operations, and useful methods for PTP packet encoding/decoding. Uses little-endian as default
  */
-export class PTPDataView extends DataView {
-	private currentReadOffset = 0
+export class PTPDataView {
+	private currentByteOffset = 0
+	private currentByteLength = 0
+
+	private dataView: DataView
+	private buffer: ArrayBufferLike
 
 	public constructor(
-		buffer: ArrayBufferLike,
-		byteOffset?: number,
-		byteLength?: number
+		bufferOrSize: ArrayBufferLike | number = DEFAULT_BUFFER_SIZE
 	) {
-		super(buffer, byteOffset, byteLength)
+		if (typeof bufferOrSize === 'number') {
+			this.buffer = new ArrayBuffer(bufferOrSize)
+		} else {
+			this.buffer = bufferOrSize
+		}
+
+		this.dataView = new DataView(this.buffer)
 	}
 
 	public skip(bytes: number) {
-		if (this.currentReadOffset + bytes > this.byteLength) {
+		if (this.currentByteOffset + bytes > this.buffer.byteLength) {
 			throw new Error('Not enough byteLength to skip')
 		}
-		this.currentReadOffset += bytes
+		this.currentByteOffset += bytes
 
 		return this
 	}
 
 	public goto(offset: number) {
-		this.currentReadOffset = offset
+		this.currentByteOffset = offset
 		return this
 	}
 
 	public readUint8 = () => {
-		const off = this.currentReadOffset
-		this.currentReadOffset += 1
-		return this.getUint8(off)
+		const off = this.currentByteOffset
+		this.currentByteOffset += 1
+		return this.dataView.getUint8(off)
 	}
 
 	public readInt8 = () => {
-		const off = this.currentReadOffset
-		this.currentReadOffset += 1
-		return this.getInt8(off)
+		const off = this.currentByteOffset
+		this.currentByteOffset += 1
+		return this.dataView.getInt8(off)
 	}
 
 	public readUint16 = () => {
-		const off = this.currentReadOffset
-		this.currentReadOffset += 2
-		return this.getUint16(off, true)
+		const off = this.currentByteOffset
+		this.currentByteOffset += 2
+		return this.dataView.getUint16(off, true)
 	}
 
 	public readInt16 = () => {
-		const off = this.currentReadOffset
-		this.currentReadOffset += 2
-		return this.getInt16(off, true)
+		const off = this.currentByteOffset
+		this.currentByteOffset += 2
+		return this.dataView.getInt16(off, true)
 	}
 
 	public readUint32 = () => {
-		const off = this.currentReadOffset
-		this.currentReadOffset += 4
-		return this.getUint32(off, true)
+		const off = this.currentByteOffset
+		this.currentByteOffset += 4
+		return this.dataView.getUint32(off, true)
 	}
 
 	public readUint64 = (): bigint => {
-		const off = this.currentReadOffset
-		this.currentReadOffset += 8
-		return this.getBigUint64(off, true)
+		const off = this.currentByteOffset
+		this.currentByteOffset += 8
+		return this.dataView.getBigUint64(off, true)
 	}
 
 	public readAsciiString = (): string => {
-		const start = this.currentReadOffset
+		const start = this.currentByteOffset
 		let end = start
 
-		for (end = start; end < this.byteLength; end++) {
-			if (this.getUint8(end) === 0x00) break
+		for (end = start; end < this.buffer.byteLength; end++) {
+			if (this.dataView.getUint8(end) === 0x00) break
 		}
 
 		const chunk = this.buffer.slice(start, end)
 
-		this.currentReadOffset = end + 1
+		this.currentByteOffset = end + 1
 
 		const str = String.fromCharCode(...new Uint8Array(chunk))
 		return str
@@ -89,11 +99,11 @@ export class PTPDataView extends DataView {
 		}
 
 		const strBuffer = this.buffer.slice(
-			this.currentReadOffset,
-			this.currentReadOffset + (strLen - 1) * 2
+			this.currentByteOffset,
+			this.currentByteOffset + (strLen - 1) * 2
 		)
 
-		this.currentReadOffset += strLen * 2
+		this.currentByteOffset += strLen * 2
 
 		const str = String.fromCharCode(...new Uint16Array(strBuffer))
 		return str
@@ -103,7 +113,7 @@ export class PTPDataView extends DataView {
 		this.skip(2)
 
 		let str = ''
-		while (this.currentReadOffset < this.byteLength) {
+		while (this.currentByteOffset < this.dataView.byteLength) {
 			const charCode = this.readUint16()
 			if (charCode === 0x0000) break
 
@@ -135,11 +145,74 @@ export class PTPDataView extends DataView {
 	}
 
 	public peekRest = (): ArrayBuffer => {
-		return this.buffer.slice(this.currentReadOffset)
+		return this.buffer.slice(this.currentByteOffset)
 	}
 
 	private readArray = (getFunc: () => number) => {
 		const length = this.readUint32()
 		return _.times(length, getFunc.bind(this))
+	}
+
+	// Write methods
+	public writeUint8(value: number) {
+		this.resizeBuffer(this.currentByteOffset + 1)
+		this.dataView.setUint8(this.currentByteOffset, value)
+		this.currentByteOffset += 1
+		this.currentByteLength = Math.max(this.currentByteOffset)
+	}
+
+	public writeInt8(value: number) {
+		this.resizeBuffer(this.currentByteOffset + 1)
+		this.dataView.setInt8(this.currentByteOffset, value)
+		this.currentByteOffset += 1
+		this.currentByteLength = Math.max(this.currentByteOffset)
+	}
+
+	public writeUint16(value: number) {
+		this.resizeBuffer(this.currentByteOffset + 2)
+		this.dataView.setUint16(this.currentByteOffset, value, true)
+		this.currentByteOffset += 2
+		this.currentByteLength = Math.max(this.currentByteOffset)
+	}
+
+	public writeInt16(value: number) {
+		this.resizeBuffer(this.currentByteOffset + 2)
+		this.dataView.setInt16(this.currentByteOffset, value, true)
+		this.currentByteOffset += 2
+		this.currentByteLength = Math.max(this.currentByteOffset)
+	}
+
+	public writeUint32(value: number) {
+		this.resizeBuffer(this.currentByteOffset + 4)
+		this.dataView.setUint32(this.currentByteOffset, value, true)
+		this.currentByteOffset += 4
+		this.currentByteLength = Math.max(this.currentByteOffset)
+	}
+
+	public writeInt32(value: number) {
+		this.resizeBuffer(this.currentByteOffset + 4)
+		this.dataView.setInt32(this.currentByteOffset, value, true)
+		this.currentByteOffset += 4
+		this.currentByteLength = Math.max(this.currentByteOffset)
+	}
+
+	public toBuffer() {
+		return this.buffer.slice(0, this.currentByteLength)
+	}
+
+	private resizeBuffer(byteLength: number) {
+		if (byteLength <= this.buffer.byteLength) {
+			return
+		}
+
+		const oldBufferArray = new Uint8Array(this.buffer)
+
+		// Reallocate buffer then initialize dataView
+		this.buffer = new ArrayBuffer(byteLength)
+		this.dataView = new DataView(this.buffer)
+
+		// Copy all existing data to newly allocated buffer
+		const newBufferArray = new Uint8Array(this.buffer)
+		newBufferArray.set(oldBufferArray, 0)
 	}
 }
