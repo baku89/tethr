69ecba6df6b34b4f161f0dba5b3d2ddd1a14f254
Baku Hashimoto <mail@baku89.com>
2021-09-28 15:26:41 +0900

    Merge PTP constants into PTPDatacode

--- src/PTPDatacode.ts
+++ src/PTPDatacode.ts
@@ -174,3 +174,24 @@ export enum DatatypeCode {
 	Uint128Array = 0x400a,
 	String = 0xffff,
 }
+
+export enum PTPStorageTypeCode {
+	Undefined = 0x0000,
+	FixedROM = 0x0001,
+	RemovableROM = 0x0002,
+	FixedRAM = 0x0003,
+	RemovableRAM = 0x0004,
+}
+
+export enum PTPFilesystemTypeCode {
+	Undefined = 0x0000,
+	GenericFlat = 0x0001,
+	GenericHierarchical = 0x0002,
+	DCF = 0x0003,
+}
+
+export enum PTPAccessCapabilityCode {
+	ReadWrite = 0x0000,
+	ReadOnlyWithoutObjectDeletion = 0x0001,
+	ReadOnlyWithObjectDeletion = 0x0002,
+}
