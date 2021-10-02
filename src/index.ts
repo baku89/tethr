06be54bb2b5a64261ed88146bcf1b13854997018
Baku Hashimoto <mail@baku89.com>
2021-10-02 14:21:59 +0900

    Expose some types to package index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,48 @@
-import {ConfigType} from '../src/configs'
+import {
+	Aperture,
+	BatteryLevel,
+	ConfigName,
+	ConfigType,
+	DriveMode,
+	ExposureMeteringMode,
+	ExposureMode,
+	FlashMode,
+	FocalLength,
+	FocusMeteringMode,
+	FocusMode,
+	FunctionalMode,
+	ISO,
+	ManualFocusOption,
+	WhiteBalance,
+} from '../src/configs'
 import {detectTethr} from './detectTethr'
-import {ConfigDesc, Tethr} from './Tethr'
+import {
+	ConfigDesc,
+	OperationResult,
+	OperationResultStatus,
+	Tethr,
+} from './Tethr'
 
-export {detectTethr, Tethr, ConfigType, ConfigDesc}
+export {
+	detectTethr,
+	Tethr,
+	ConfigType,
+	ConfigName,
+	ConfigDesc,
+	OperationResult,
+	OperationResultStatus,
+	// Config Types
+	Aperture,
+	BatteryLevel,
+	DriveMode,
+	ExposureMeteringMode,
+	ExposureMode,
+	FlashMode,
+	FocusMode,
+	FunctionalMode,
+	FocusMeteringMode,
+	FocalLength,
+	ISO,
+	ManualFocusOption,
+	WhiteBalance,
+}
