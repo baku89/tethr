433c2fa5780c0b3f8854a47b75fdebe79b91b295
Baku Hashimoto <mail@baku89.com>
2021-10-02 18:45:39 +0900

    Revert "Install get-user-media-promise and prelare d.ts"
    This reverts commit f05c927e1ad4a8607c733e0ad702fdaee88d2044.

--- src/modules.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'get-user-media-promise' {
-	interface GetUserMediaPromise {
-		(constraints: MediaStreamConstraints): Promise<MediaStream>
-		isSupported: boolean
-	}
-	const getUserMedia: GetUserMediaPromise
-	export default getUserMedia
-}
