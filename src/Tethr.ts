1bf9dc728afd3942c55dea5ca837c28af6b952af
Baku Hashimoto <mail@baku89.com>
2023-10-12 03:27:58 +0900

    Delete logging

--- src/Tethr.ts
+++ src/Tethr.ts
@@ -151,7 +151,6 @@ export abstract class Tethr
 
 		for (const [name, value] of sortedConfigs) {
 			// NOTE: this might be converted to parallel execution in the future
-			console.log('setting', name, value)
 			await this.set(name, value)
 
 			// The delay is necessary to avoid "busy" error
